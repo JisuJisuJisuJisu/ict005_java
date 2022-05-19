a895d0270cb8d2fde3c3182bacfcfeaff80917f3
JisuJisuJisuJisu <105847303+JisuJisuJisuJisu@users.noreply.github.com>
2022-05-19 16:59:37 +0900

    first class java project
    yeahahah~~

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕하세요?");
+		System.out.println("자바를 처음 공부합니다.");
+		System.out.println("나이는 20살입니다.");
+	}
+
+}
